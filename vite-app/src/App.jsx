a0393be03c75737d43b4ae298d9f21f7732dff08
netneko <cecizucchino@gmail.com>
2023-04-28 10:55:12 -0400

    me olvide la extension en el ultimo import

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import NavBar from './components/NavBar/Navbar';
+import NavBar from './components/NavBar/Navbar.jsx';
 
 function App() 
 {
