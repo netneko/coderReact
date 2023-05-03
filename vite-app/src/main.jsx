3045d288e7fa19fbb334ff1404a2cb823cbb0701
netneko <cecizucchino@gmail.com>
2023-05-03 18:42:36 -0400

    estilos

--- vite-app/src/main.jsx
+++ vite-app/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import * as bootrsap from "bootstrap";
+import './index.css';
+/*import * as bootrsap from "bootstrap";*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
