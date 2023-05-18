4f419930652b8583efcb6e27ea272454f408dd78
netneko <cecizucchino@gmail.com>
2023-05-18 19:02:40 -0400

    Algo de routing

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemCount from './components/ItemCount/ItemCount.jsx';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 /*import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";*/
--- vite-app/src/components/NavBar/NavBar.jsx
+++ vite-app/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import './NavBar.css'
 import CartWidget from '../CartWidget/CartWidget'
 import React from "react";
+import {Link, NavLink} from 'react-router-dom';
 
 
 const NavBar = () => {
