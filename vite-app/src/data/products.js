aafebfc28ce522ffde69703b2932cefc0374993a
netneko <cecizucchino@gmail.com>
2023-05-30 18:02:34 -0400

    cambios de estilo y nuevos productos

--- vite-app/src/data/products.js
+++ vite-app/src/data/products.js
@@ -1,76 +1,148 @@
 const productsDB = [
-    {
-      id: 1,
-      title: "Sal del Himalaya",
-      category: "Sales",
-      stock: 10,
-      image: "/assets/sal_himalaya.jpg",
-      price: 9.99,
-      description: "Sal rosada del Himalaya, rica en minerales y con un sabor único."
-    },
-    {
-      id: 2,
-      title: "Pimienta Negra",
-      category: "Especias",
-      stock: 15,
-      image: "/assets/pimienta_negra.jpg",
-      price: 5.99,
-      description: "Pimienta negra de alta calidad, ideal para sazonar tus platos favoritos."
-    },
-    {
-      id: 3,
-      title: "Curry en Polvo",
-      category: "Especias",
-      stock: 8,
-      image: "/assets/curry_polvo.jpg",
-      price: 7.5,
-      description: "Mezcla de especias en polvo perfecta para darle un toque exótico a tus comidas."
-    },
-    {
-      id: 4,
-      title: "Ras el Hanout",
-      category: "Rubs",
-      stock: 5,
-      image: "/assets/ras_el_hanout.jpg",
-      price: 12.99,
-      description: "Mezcla de especias marroquí para condimentar carnes y platos tradicionales."
-    },
-    {
-      id: 5,
-      title: "Sal de Trufa Negra",
-      category: "Sales",
-      stock: 12,
-      image: "/assets/sal_trufa_negra.jpg",
-      price: 14.99,
-      description: "Sal gourmet con auténtico aroma y sabor a trufa negra, perfecta para realzar tus platos."
-    },
-    {
-      id: 6,
-      title: "Za'atar",
-      category: "Especias",
-      stock: 20,
-      image: "/assets/zaatar.jpg",
-      price: 8.5,
-      description: "Mezcla de especias mediterráneas para aderezar panes, carnes y ensaladas."
-    },
-    {
-      id: 7,
-      title: "Rústico Italiano",
-      category: "Rubs",
-      stock: 3,
-      image: "/assets/rustico_italiano.jpg",
-      price: 10.99,
-      description: "Mezcla de hierbas y especias italianas para darle un toque mediterráneo a tus platos."
-    },
-    {
-      id: 8,
-      title: "Chimichurri Argentino",
-      category: "Rubs",
-      stock: 6,
-      image:"/assets/chimichurri.jpg",
-      price: 9.5,
-      description: "Mezcla de hierbas y especias para preparar el clásico chimichurri argentino."
-    }
-  ];
-  
-  export default productsDB;
+  {
+    id: 1,
+    title: "Sal del Himalaya",
+    category: "Sales",
+    stock: 10,
+    image: "/assets/sal_himalaya.jpg",
+    price: 9.99,
+    description: "Sal rosada del Himalaya, rica en minerales y con un sabor único."
+  },
+  {
+    id: 2,
+    title: "Pimienta Negra",
+    category: "Especias",
+    stock: 15,
+    image: "/assets/pimienta_negra.jpg",
+    price: 5.99,
+    description: "Pimienta negra de alta calidad, ideal para sazonar tus platos favoritos."
+  },
+  {
+    id: 3,
+    title: "Curry en Polvo",
+    category: "Especias",
+    stock: 8,
+    image: "/assets/curry_polvo.jpg",
+    price: 7.5,
+    description: "Mezcla de especias en polvo perfecta para darle un toque exótico a tus comidas."
+  },
+  {
+    id: 4,
+    title: "Ras el Hanout",
+    category: "Rubs",
+    stock: 5,
+    image: "/assets/ras_el_hanout.jpg",
+    price: 12.99,
+    description: "Mezcla de especias marroquí para condimentar carnes y platos tradicionales."
+  },
+  {
+    id: 5,
+    title: "Sal de Trufa Negra",
+    category: "Sales",
+    stock: 12,
+    image: "/assets/sal_trufa_negra.jpg",
+    price: 14.99,
+    description: "Sal gourmet con auténtico aroma y sabor a trufa negra, perfecta para realzar tus platos."
+  },
+  {
+    id: 6,
+    title: "Za'atar",
+    category: "Especias",
+    stock: 20,
+    image: "/assets/zaatar.jpg",
+    price: 8.5,
+    description: "Mezcla de especias mediterráneas para aderezar panes, carnes y ensaladas."
+  },
+  {
+    id: 7,
+    title: "Rústico Italiano",
+    category: "Rubs",
+    stock: 3,
+    image: "/assets/rustico_italiano.jpg",
+    price: 10.99,
+    description: "Mezcla de hierbas y especias italianas para darle un toque mediterráneo a tus platos."
+  },
+  {
+    id: 8,
+    title: "Chimichurri Argentino",
+    category: "Rubs",
+    stock: 6,
+    image: "/assets/chimichurri.jpg",
+    price: 9.5,
+    description: "Mezcla de hierbas y especias para preparar el clásico chimichurri argentino."
+  },
+  {
+    id: 9,
+    title: "Chicken Rub",
+    category: "Rubs",
+    stock: 10,
+    image: "/assets/chicken_rub.jpg",
+    price: 7.99,
+    description: "Mezcla de especias para sazonar pollo a la parrilla o al horno."
+  },
+  {
+    id: 10,
+    title: "Pork Rub",
+    category: "Rubs",
+    stock: 8,
+    image: "/assets/pork_rub.jpg",
+    price: 7.99,
+    description: "Mezcla de especias para darle sabor y jugosidad a las preparaciones de cerdo."
+  },
+  {
+    id: 11,
+    title: "Sal Mostaza",
+    category: "Sales",
+    stock: 15,
+    image: "/assets/sal_mostaza.jpg",
+    price: 3.99,
+    description: "Sal con un toque de mostaza que realza el sabor de tus platos favoritos."
+  },
+  {
+    id: 12,
+    title: "Sal Picante",
+    category: "Sales",
+    stock: 6,
+    image: "/assets/sal_picante.jpg",
+    price: 3.99,
+    description: "Sal con un toque picante para añadir un poco de calidez a tus comidas."
+  },
+  {
+    id: 13,
+    title: "Azafrán",
+    category: "Especias",
+    stock: 4,
+    image: "/assets/azafran.jpg",
+    price: 15.99,
+    description: "Especia valiosa y aromática que agrega un sabor único y un color vibrante a tus platos."
+  },
+  {
+    id: 14,
+    title: "Azafrán Molido",
+    category: "Especias",
+    stock: 2,
+    image: "/assets/azafran_molido.jpg",
+    price: 12.99,
+    description: "Azafrán molido para una fácil incorporación en tus recetas y obtener su distintivo sabor y color."
+  },
+  {
+    id: 15,
+    title: "Ajo en Polvo",
+    category: "Especias",
+    stock: 18,
+    image: "/assets/ajo_polvo.jpg",
+    price: 4.99,
+    description: "Ajo en polvo para realzar el sabor de tus platos con un toque de ajo."
+  },
+  {
+    id: 16,
+    title: "Beef Rub",
+    category: "Rubs",
+    stock: 9,
+    image: "/assets/beef_rub.jpg",
+    price: 8.99,
+    description: "Mezcla de especias para realzar el sabor de tus preparaciones de carne de res."
+  }
+];
+
+export default productsDB;
