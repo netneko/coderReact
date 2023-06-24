d85ed6e72b768c502cb852ea30a5d0cd74dd5cad
netneko <cecizucchino@gmail.com>
2023-06-24 18:40:21 -0400

    validaciones agregadas

--- vite-app/src/components/Cart/Cart.jsx
+++ vite-app/src/components/Cart/Cart.jsx
@@ -5,30 +5,43 @@ import { useNavigate } from "react-router-dom";
 import { createOrder } from "../../services/firebase/firebaseConfig";
 import CheckoutForm from '../CheckoutForm/CheckoutForm';
 
-
-
-
-
 const Cart = () => {
-    const { cart, totalItems, countTotalPrice, clearCart, removeItem, subTotalItem } = useContext(CartContext);
+    const { cart, totalItems, countTotalPrice, clearCart, removeItem } = useContext(CartContext);
     const navigateTo = useNavigate();
     const [buyerName, setBuyerName] = useState("");
     const [buyerPhone, setBuyerPhone] = useState("");
     const [buyerEmail, setBuyerEmail] = useState("");
-    //Esto es innecesario en realidad porque nunca voy a mostrar el icono del carrito vacio y por lo tanto no puedo acceder al mismo si esta vacio
-    if (totalItems === 0) {
-        return (
-            <div>
-                <h1>No hay items en el carrito!</h1>
-                <Link to="/" className="Option">
-                    Volver al listado de productos
-                </Link>
-            </div>
-        );
-    }
+    const [error, setError] = useState("");
 
-    async function handleConfirm(buyerName, buyerPhone, buyerEmail) {
-       // console.log(buyerName); 
+    // Validar campos antes de confirmar la compra
+    const validateFields = () => {
+        if (buyerName.trim() === "" || buyerPhone.trim() === "" || buyerEmail.trim() === "") {
+            setError("Todos los campos son requeridos");
+            return false;
+        }
+
+        // Validar formato de email
+        const emailRegex = /^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,})$/;
+        if (!emailRegex.test(buyerEmail)) {
+            setError("El formato del email no es válido");
+            return false;
+        }
+
+        // Validar campo de teléfono como numérico
+        if (isNaN(buyerPhone)) {
+            setError("El teléfono debe ser numérico");
+            return false;
+        }
+
+        setError("");
+        return true;
+    };
+
+    async function handleConfirm() {
+        if (!validateFields()) {
+            return;
+        }
+
         const order = {
             items: cart,
             buyer: {
@@ -45,56 +58,61 @@ const Cart = () => {
             clearCart();
             navigateTo(`/order-confirmation/${id}`);
         } catch (error) {
-            alert(error); 
+            alert(error);
         }
     }
-
-
 
     return (
         <div>
             <h2>Tu carrito de compras</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Cantidad</th>
-                        <th>Subtotal</th>
-                        <th>Remover</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {cart.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.title}</td>
-                            <td>{item.quantity}</td>
-                            <td>Subtotal: {item.quantity * item.price}</td>
-                            <td>
-                                <button onClick={() => removeItem(item.id)}>X</button>
-                            </td>
-                        </tr>
-                    ))}
-                    <tr>
-                        <td colSpan="2">Total del carrito:</td>
-                        <td colSpan="2">{countTotalPrice()}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <button onClick={() => clearCart()}>Vaciar carrito</button>
-            <CheckoutForm
-                setBuyerName={setBuyerName}
-                setBuyerPhone={setBuyerPhone}
-                setBuyerEmail={setBuyerEmail}
-            />
-            <button onClick={() => handleConfirm(buyerName, buyerPhone, buyerEmail)}>Crear orden de compra</button>
+            {totalItems === 0 ? (
+                <div>
+                    <h1>No hay items en el carrito!</h1>
+                    <Link to="/" className="Option">
+                        Volver al listado de productos
+                    </Link>
+                </div>
+            ) : (
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Título</th>
+                                <th>Cantidad</th>
+                                <th>Subtotal</th>
+                                <th>Remover</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cart.map((item) => (
+                                <tr key={item.id}>
+                                    <td>{item.title}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>Subtotal: {item.quantity * item.price}</td>
+                                    <td>
+                                        <button onClick={() => removeItem(item.id)}>X</button>
+                                    </td>
+                                </tr>
+                            ))}
+                            <tr>
+                                <td colSpan="2">Total del carrito:</td>
+                                <td colSpan="2">{countTotalPrice()}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button onClick={clearCart}>Vaciar carrito</button>
+                    <CheckoutForm
+                        setBuyerName={setBuyerName}
+                        setBuyerPhone={setBuyerPhone}
+                        setBuyerEmail={setBuyerEmail}
+                        error={error}
+                    />
+                    {error && <p>{error}</p>}
+                    <button onClick={handleConfirm}>Crear orden de compra</button>
+                </div>
+            )}
         </div>
     );
-
-
-
 };
 
 export default Cart;
-
-
-//Me gustaria agregar los botones para agregar o restar unidades de un item
